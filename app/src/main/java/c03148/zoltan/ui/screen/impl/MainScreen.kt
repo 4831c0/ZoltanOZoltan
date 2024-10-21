80e5f0c3f4f404550de3eb022eb3f5242626c9bd
4831c0 <4831c0@proton.me>
2024-10-21 11:04:20 +0200

    Add navbar

--- app/src/main/java/c03148/zoltan/ui/screen/impl/MainScreen.kt
+++ app/src/main/java/c03148/zoltan/ui/screen/impl/MainScreen.kt
@@ -1,9 +1,27 @@
 package c03148.zoltan.ui.screen.impl
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import c03148.zoltan.R
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
+    Box(modifier.fillMaxSize()) {
+        Text(
+            text = stringResource(R.string.my_profile),
+            color = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    }
+}
 
+@Preview()
+@Composable
+fun MainScreenPreview() {
+    MainScreen(Modifier.fillMaxSize())
 }
