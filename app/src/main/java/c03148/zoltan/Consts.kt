33e4adf892681f95cd0a794c250ec60be0e048a1
4831c0 <4831c0@proton.me>
2024-12-16 11:26:50 +0100

    Change host URL

--- app/src/main/java/c03148/zoltan/Consts.kt
+++ app/src/main/java/c03148/zoltan/Consts.kt
@@ -1,6 +1,6 @@
 package c03148.zoltan
 
-const val HOST = "http://192.168.1.158:3005"
+const val HOST = "https://zoltan.cemrk.me"
 const val MAP = "$HOST/map"
 const val zoltanList = "$HOST/api/zoltan/list"
 const val samplePfp = "$HOST/res/profile.jpeg"
